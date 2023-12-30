1f3596e4e283d8a1da948abdbcfcc4ca4c7f6146
zzfima <zzfima@gmail.com>
2023-12-30 16:41:31 +0200

    add tea maker

---
+++ AdvancedAsync/AdvancedAsync/Extentions.cs
@@ -0,0 +1,7 @@
+﻿public static class Extentions
+{
+    public static void Dump(this string str)
+    {
+        Console.WriteLine(str);
+    }
+}
---
+++ AdvancedAsync/AdvancedAsync/TeaMaker.cs
@@ -0,0 +1,29 @@
+﻿internal class TeaMaker
+{
+    private async static Task Main(string[] args)
+    {
+        var res = await MakeTeaAsync();
+        Console.WriteLine(res);
+
+        Console.ReadLine();
+    }
+
+    static async Task<string> MakeTeaAsync()
+    {
+        var boilingWater = BoilWaterAsync();
+        $"3. take the cups out {Thread.CurrentThread.ManagedThreadId}".Dump();
+        $"4. put tea in cups {Thread.CurrentThread.ManagedThreadId}".Dump();
+        var water = await boilingWater;
+        var tea = $"6. pour {water} in cups {Thread.CurrentThread.ManagedThreadId}";
+        return tea;
+    }
+
+    static async Task<string> BoilWaterAsync()
+    {
+        $"1. start the kettle {Thread.CurrentThread.ManagedThreadId}".Dump();
+        $"2. waiting for the kettle {Thread.CurrentThread.ManagedThreadId}".Dump();
+        await Task.Delay(3000);
+        $"5. kettle finished boiling {Thread.CurrentThread.ManagedThreadId}".Dump();
+        return $"boiled water";
+    }
+}
