39647e8e85f59a282a712933289da484dd3ecf04
zzfima <zzfima@gmail.com>
2024-01-08 16:20:53 +0200

    rfct

---
+++ AdvancedAsync/Demo5BlockedTask/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo5BlockedTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt.Text = "waiting";
+            DelayAsync().Wait();
+            txt.Text = "waiting complete";
+        }
+
+        async Task DelayAsync()
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
