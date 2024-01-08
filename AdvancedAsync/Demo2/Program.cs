34f92b48e2a4e4571d3848da4057b19060d5bafb
zzfima <zzfima@gmail.com>
2024-01-08 10:13:38 +0200

    from https://www.youtube.com/watch?v=lh8cT6qI-nA&ab_channel=DotNext

---
+++ AdvancedAsync/Demo2/Program.cs
@@ -0,0 +1,36 @@
+﻿//https://www.youtube.com/watch?v=lh8cT6qI-nA&ab_channel=DotNext
+
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Task.Run(MainAsync);
+
+        Console.ReadLine();
+    }
+
+    static async Task MainAsync()
+    {
+        Console.WriteLine("Started");
+
+        try
+        {
+            DelayAndThrowAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception happens {e.Message}");
+        }
+
+        Console.WriteLine("Finished");
+    }
+
+    static async void DelayAndThrowAsync()
+    {
+        await Task.Delay(1000);
+        throw new InvalidOperationException("fff");
+    }
+}
---
+++ AdvancedAsync/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿//https://www.youtube.com/watch?v=lh8cT6qI-nA&ab_channel=DotNext
+
+using System.Diagnostics;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            printDebug("Button_Click1 start");
+            await Task.Run(WaitAsync);
+            printDebug("Button_Click1 finish");
+        }
+
+        private async void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            printDebug("Button_Click2 start");
+            await WaitAsync();
+            printDebug("Button_Click2 finish");
+        }
+
+        private async Task WaitAsync()
+        {
+            printDebug("WaitAsync start");
+            await Task.Delay(5000);
+            printDebug("WaitAsync finish");
+        }
+
+        private void printDebug(string title)
+        {
+            Debug.WriteLine("***********************");
+            Debug.WriteLine(title);
+            Debug.WriteLine("ManagedThreadId: " + Thread.CurrentThread.ManagedThreadId);
+            Debug.WriteLine("Task.CurrentId: " + Task.CurrentId);
+            Debug.WriteLine("SynchronizationContext: " + SynchronizationContext.Current?.ToString());
+            Debug.WriteLine("***********************");
+        }
+    }
+}
